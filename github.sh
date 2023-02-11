6ef87c8ca6e6393d129d69e000ad9f391661f17c
arun-selva-02136 <arunakilselva@gmail.com>
2023-02-11 14:06:22 +0530

    first commit

--- github.sh
+++ github.sh
@@ -46,7 +46,7 @@ echo " done."
 # push to remote repo
 echo "Pushing to remote ..."
 git init
-git add -A
+git add *
 git commit -m "first commit"
 git remote rm origin
 git remote add origin git@github.com:$username/$reponame.git
